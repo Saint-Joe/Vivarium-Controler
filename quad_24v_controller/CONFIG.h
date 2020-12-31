38fa5e989d60843ce757f6e31b79952ef102723e
Keith Talbot <7109623+mad-science-lab@users.noreply.github.com>
2020-12-30 22:07:41 -0500

    Create CONFIG.h

---
+++ quad_24v_controller/CONFIG.h
@@ -0,0 +1,17 @@
+/************************* Application Info **********************************/
+
+#define APP_NAME        "Quad Controller"
+#define CODE_VER        "00.01"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "...ssid..."
+#define WLAN_PASS       "...password..."
+
+/***************************** MQTT Setup ************************************/
+
+#define MQTT_SERVER      "192.168.1.100"       //MQTT Broker host name (mqtt.domain.com) or ip
+#define MQTT_SERVERPORT  1883                   // use 8883 for SSL
+#define MQTT_USERNAME    "...mqttuser..."
+#define MQTT_PASSWORD    "...mqttpass..."
+#define UNIQUE_ID        "...QUAD24TEST..."          //user defined id, useful if you have many sensor packs
